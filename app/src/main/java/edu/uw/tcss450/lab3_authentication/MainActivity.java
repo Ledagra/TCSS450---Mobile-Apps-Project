fe303c19d9bdab45fe1145765faf576faf5fa782
Ledagra <svanluven90@gmail.com>
2023-04-30 16:47:51 -0700

    Bottom Nav Menu and Profile Menu added

--- app/src/main/java/edu/uw/tcss450/lab3_authentication/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/lab3_authentication/MainActivity.java
@@ -2,12 +2,22 @@ package edu.uw.tcss450.lab3_authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.window.SplashScreen;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.uw.tcss450.lab3_authentication.model.UserInfoViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        //Passing each menu ID as a set of Ids because each
+        //menu should be considered as top level destinations
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_messages, R.id.navigation_weather,
+                R.id.navigation_friends).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
     }
 }
