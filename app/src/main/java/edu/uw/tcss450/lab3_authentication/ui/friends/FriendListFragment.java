ca88296093caf33f713393ae9d268a7e087e678c
Ledagra <svanluven90@gmail.com>
2023-05-02 14:30:19 -0700

    Friend list updates

--- app/src/main/java/edu/uw/tcss450/lab3_authentication/ui/friends/FriendListFragment.java
+++ app/src/main/java/edu/uw/tcss450/lab3_authentication/ui/friends/FriendListFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import edu.uw.tcss450.lab3_authentication.R;
 
@@ -16,10 +18,17 @@ import edu.uw.tcss450.lab3_authentication.R;
 public class FriendListFragment extends Fragment {
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        ListView listView;
+        String[] friendList = {"Steve", "Bob", "Chuck", "Larry", "Ethel", "Gertrude", "Hilda"};
+
+        listView = (ListView) getView().findViewById(R.id.navigation_friends);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>();
+        listView.setAdapter(arrayAdapter);
         return inflater.inflate(R.layout.fragment_friend_list, container, false);
     }
 }
