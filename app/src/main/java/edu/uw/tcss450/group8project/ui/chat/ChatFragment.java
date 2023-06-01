2631df5b030188133ea92eee54e30d0ebe1f12bd
Andrew Lau Ho YIn <70436793+andrewlau019@users.noreply.github.com>
2023-06-01 13:52:03 -0700

    create delete and clear buttons
    have it visually but not yet implemented functionally

--- app/src/main/java/edu/uw/tcss450/group8project/ui/chat/ChatFragment.java
+++ app/src/main/java/edu/uw/tcss450/group8project/ui/chat/ChatFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import edu.uw.tcss450.group8project.R;
 
@@ -22,5 +25,39 @@ public class ChatFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.chat_fragment, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //onclick send
+        ImageButton sendButton = view.findViewById(R.id.button_send);
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //send message
+            }
+        });
+
+        //onclick clear
+        ImageButton clearButton = view.findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clear all messages
+            }
+        });
+
+        //onclick delete
+        ImageButton deleteButton = view.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //delete the chatroom
+            }
+        });
+
     }
 }
--- app/src/main/java/edu/uw/tcss450/group8project/ui/chat/CreatChatRoomDialog.java
+++ app/src/main/java/edu/uw/tcss450/group8project/ui/chat/CreatChatRoomDialog.java
@@ -14,36 +14,47 @@ import java.util.ArrayList;
 import edu.uw.tcss450.group8project.R;
 import edu.uw.tcss450.group8project.ui.friends.Constants;
 import edu.uw.tcss450.group8project.ui.friends.Friend;
+import edu.uw.tcss450.group8project.ui.messages.Message;
 import edu.uw.tcss450.group8project.ui.messages.MessageInfo;
 
 public class CreatChatRoomDialog extends DialogFragment {
 
     private ArrayList<Friend> selectedList;
     private ArrayList<Friend> mFriend = Constants.getFriendData();
+    private ArrayList<Message> mMessage;
+
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         selectedList = new ArrayList<>();
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Convert ArrayList to string array
+        String[] friendNames = new String[mFriend.size()];
+        for (int i = 0; i < mFriend.size(); i++) {
+            friendNames[i] = mFriend.get(i).getName(); // Replace 'getName()' with the appropriate method to get the friend's name
+        }
+
+
         //set dialog title
         builder.setTitle("Select Contacts")
-//                .setMultiChoiceItems(mFriend, null, new DialogInterface.OnMultiChoiceClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-//                        if(isChecked){
-//                            //user selected the item, add it to the selected item
-//                            selectedList.add(mFriend.get(which));
-//                        } else if(selectedList.contains(mFriend.get(which))){
-//                            //item already in the array, remove it (uncheck)
-//                            selectedList.remove(mFriend.get(which));
-//                        }
-//                    }
-//                })
+                .setMultiChoiceItems(friendNames, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        if(isChecked){
+                            //user selected the item, add it to the selected item
+                            selectedList.add(mFriend.get(which));
+                        } else if(selectedList.contains(mFriend.get(which))){
+                            //item already in the array, remove it (uncheck)
+                            selectedList.remove(mFriend.get(which));
+                        }
+                    }
+                })
                 .setPositiveButton("Create Chatroom", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //create chatroom with new contacts
+                        //add it into the
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
